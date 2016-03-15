1f384d84b818133e0f35c182b142a97c9d302f3e
gittiggit <mailtommrr@gmail.com>
2016-03-15 18:24:15 +0600

    Commited after implementing login logout feature for the user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,23 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
-    
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in(@user)
+      flash[:success] = "You are logged in successfully"
+      redirect_to users_path
+    else
+      flash[:notice] = "Please enter correct credential"
+      render :new
+    end
   end
+
+  def destroy
+    logout
+    redirect_to root_url
+  end
+
 end

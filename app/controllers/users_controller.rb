914084ae515f88fc9456cb9560fbacb94313eabe
gittiggit <mailtommrr@gmail.com>
2016-03-15 18:24:15 +0600

    Commited after implementing login logout feature for the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def show
@@ -27,7 +28,6 @@ class UsersController < ApplicationController
   end
 
   private
-
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
